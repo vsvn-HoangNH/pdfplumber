944eaedf2d0a6a06910940dbf9f5960005fafedb
Jeremy Singer-Vine <jsvine@gmail.com>
2024-07-06 16:18:51 -0400

    Remove testing for Python 3.8, add for 3.12

--- setup.py
+++ setup.py
@@ -50,9 +50,9 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
 )
